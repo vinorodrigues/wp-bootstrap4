9f8945094c3d86a40f07030804654644c6a7d37a
Vino Rodrigues <github@m.vinorodrigues.com>
2016-05-11 21:43:08 +1000

    .hidden -> .hidden-all

--- footer.php
+++ footer.php
@@ -95,6 +95,6 @@ endif;
 
 <?php echo '</div>'; // .folio or .footer ?>
 
-<div class="hidden" hidden><?php wp_footer(); ?></div>
+<div class="hidden-all" hidden><?php wp_footer(); ?></div>
 </body>
 </html>
