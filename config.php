2e2ac4acc121a170f68b0c3dc1add9838684ca5e
Vino Rodrigues <github@m.vinorodrigues.com>
2016-09-24 21:35:33 +1000

    jq311

--- config.php
+++ config.php
@@ -4,7 +4,7 @@
  */
 
 define( 'BOOTSTRAP_VERSION', '4.0.0-alpha.4' );  // Bootstrap Version - @see http://v4-alpha.getbootstrap.com/
-define( 'JQUERY_VERSION', '3.1.0' );  // JQuery Version - @see http://jquery.com
+define( 'JQUERY_VERSION', '3.1.1' );  // JQuery Version - @see http://jquery.com
 define( 'TETHER_VERSION', '1.3.3' );  // Tether Version - @see http://tether.io
 define( 'FONTAWESOME_VERSION', '4.6.3' );  // Fontawesome version - @see http://fontawesome.io
 define( 'MATCH_HEIGHT_VERSION', '0.7.0' );  // matchHeight.js - @see http://brm.io/jquery-match-height
@@ -23,4 +23,4 @@ define( 'FEATURED_IMAGE_CLASS', 'img-rounded' );
 define( 'USE_WP45_LOGO', function_exists('get_custom_logo') );
 
 define( 'USE_ONEPAGE', true );
-define( 'USE_WOOCOMMERCE', file_exists('woocommerce/wc-functions.php') );
+define( 'USE_WOOCOMMERCE', file_exists(trailingslashit(dirname(__FILE__)).'woocommerce/wc-functions.php') );
